4c44f4b21c20b1edd233900f5470c7f879189868
SenorGrande <clooneyjh@gmail.com>
2022-05-05 20:12:49 +1200

    chore: remove old files

--- vite-project/README.md
+++
@@ -1,4 +0,0 @@
-Created with $ npm create vite@latest
-Then picked react-ts
-
-https://vitejs.dev/guide/#scaffolding-your-first-vite-project
--- vite-project/src/service-worker.js
+++
@@ -1,6 +0,0 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js');
-
-workbox.routing.registerRoute(
-    ({request}) => request.destination === 'image',
-    new workbox.strategies.CacheFirst()
-);
